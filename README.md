5f104eb9e143e08fff8a9da4d60fc7cf16f607a1
umiyosh <yumino99@gmail.com>
2016-11-16 16:39:10 +0900

    add Formula

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # homebrew-terraform
+
+``` bash
+brew tap umiyosh/homebrew-terraform
+brew install umiyosh/terraform/terraform
+```
---
+++ terraform.rb
@@ -0,0 +1,130 @@
+# Documentation: https://github.com/Homebrew/brew/blob/master/docs/Formula-Cookbook.md
+#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+require "language/go"
+
+class Terraform < Formula
+  desc "Tool to build, change, and version infrastructure"
+  homepage "https://www.terraform.io/"
+  url "https://releases.hashicorp.com/terraform/0.6.16/terraform_0.6.16_darwin_amd64.zip"
+  version "64"
+  sha256 "23feb79263126877e6128a03c600cd626f6691a118a474694c5ad45cc5da9366"
+
+  bottle do
+    cellar :any_skip_relocation
+    sha256 "14cbb8858867b76b3fabb2c33f1fc5b9552aa1f103b93c807e669135aadda8a2" => :sierra
+    sha256 "7b5eec03b39fab1fab16673eb4507846360bc5ad73e56e86647dbfaae08e09cd" => :el_capitan
+    sha256 "6001f3504249e13177e9c25d59e2bc5b172fa957eabf6b6edbd93add119508e0" => :yosemite
+  end
+
+  devel do
+    url "https://github.com/hashicorp/terraform/archive/v0.8.0-beta1.tar.gz"
+    sha256 "c159a78c56446b3231bce9b0e176e9f25568e3f6290135acafe333199f86644a"
+    version "0.8.0-beta1"
+  end
+
+  depends_on "go" => :build
+
+  go_resource "github.com/mitchellh/gox" do
+    url "https://github.com/mitchellh/gox.git",
+        :revision => "c9740af9c6574448fd48eb30a71f964014c7a837"
+  end
+
+  go_resource "github.com/mitchellh/iochan" do
+    url "https://github.com/mitchellh/iochan.git",
+        :revision => "87b45ffd0e9581375c491fef3d32130bb15c5bd7"
+  end
+
+  go_resource "github.com/kisielk/errcheck" do
+    url "https://github.com/kisielk/errcheck.git",
+        :revision => "9c1292e1c962175f76516859f4a88aabd86dc495"
+  end
+
+  go_resource "github.com/kisielk/gotool" do
+    url "https://github.com/kisielk/gotool.git",
+        :revision => "5e136deb9b893bbe6c8f23236ff4378b7a8a0dbb"
+  end
+
+  go_resource "golang.org/x/tools" do
+    url "https://go.googlesource.com/tools.git",
+        :revision => "26c35b4dcf6dfcb924e26828ed9f4d028c5ce05a"
+  end
+
+  def install
+    ENV["GOPATH"] = buildpath
+    # For the gox buildtool used by terraform, which doesn't need to
+    # get installed permanently
+    ENV.append_path "PATH", buildpath
+
+    terrapath = buildpath/"src/github.com/hashicorp/terraform"
+    terrapath.install Dir["*"]
+    Language::Go.stage_deps resources, buildpath/"src"
+
+    cd "src/github.com/mitchellh/gox" do
+      system "go", "build"
+      buildpath.install "gox"
+    end
+
+    cd "src/golang.org/x/tools/cmd/stringer" do
+      ENV.deparallelize { system "go", "build" }
+      buildpath.install "stringer"
+    end
+
+    cd "src/github.com/kisielk/errcheck" do
+      system "go", "build"
+      buildpath.install "errcheck"
+    end
+
+    cd terrapath do
+      # v0.6.12 - source contains tests which fail if these environment variables are set locally.
+      ENV.delete "AWS_ACCESS_KEY"
+      ENV.delete "AWS_SECRET_KEY"
+
+      # Runs format check and test suite via makefile
+      ENV.deparallelize { system "make", "test", "vet" }
+
+      # Generate release binary
+      arch = MacOS.prefer_64_bit? ? "amd64" : "386"
+      ENV["XC_OS"] = "darwin"
+      ENV["XC_ARCH"] = arch
+      system "make", "bin"
+
+      # Install release binary
+      bin.install "pkg/darwin_#{arch}/terraform"
+      zsh_completion.install "contrib/zsh-completion/_terraform"
+    end
+  end
+
+  test do
+    minimal = testpath/"minimal.tf"
+    minimal.write <<-EOS.undent
+      variable "aws_region" {
+          default = "us-west-2"
+      }
+
+      variable "aws_amis" {
+          default = {
+              eu-west-1 = "ami-b1cf19c6"
+              us-east-1 = "ami-de7ab6b6"
+              us-west-1 = "ami-3f75767a"
+              us-west-2 = "ami-21f78e11"
+          }
+      }
+
+      # Specify the provider and access details
+      provider "aws" {
+          access_key = "this_is_a_fake_access"
+          secret_key = "this_is_a_fake_secret"
+          region = "${var.aws_region}"
+      }
+
+      resource "aws_instance" "web" {
+        instance_type = "m1.small"
+        ami = "${lookup(var.aws_amis, var.aws_region)}"
+        count = 4
+      }
+    EOS
+    system "#{bin}/terraform", "graph", testpath
+  end
+end
+
